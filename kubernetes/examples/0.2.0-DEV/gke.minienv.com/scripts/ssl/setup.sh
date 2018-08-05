6c11c04b5f3ff2c41ab2a664962bac61a3a90fd0
Mark Watson <mark.watson@hotschedules.com>
2018-08-05 10:28:38 -0500

    Cleanup

--- kubernetes/examples/0.2.0-DEV/gke.minienv.com/scripts/ssl/setup.sh
+++ kubernetes/examples/0.2.0-DEV/gke.minienv.com/scripts/ssl/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 helm install --name cert-manager --namespace kube-system stable/cert-manager
-gcloud iam service-accounts create clouddns --display-name=clouddns --project=minienv-xxx
-gcloud iam service-accounts keys create ./clouddns.key.json --iam-account=clouddns@minienv-xxx.iam.gserviceaccount.com --project=minienv-xxx
-gcloud projects add-iam-policy-binding minienv-1 --member=serviceAccount:clouddns@minienv-xxx.iam.gserviceaccount.com --role=roles/dns.admin
+gcloud iam service-accounts create clouddns --display-name=clouddns --project=<YOUR_GCP_PROJECT_NAME>
+gcloud iam service-accounts keys create ./clouddns.key.json --iam-account=clouddns@<YOUR_GCP_PROJECT_NAME>.iam.gserviceaccount.com --project=<YOUR_GCP_PROJECT_NAME>
+gcloud projects add-iam-policy-binding minienv-1 --member=serviceAccount:clouddns@<YOUR_GCP_PROJECT_NAME>.iam.gserviceaccount.com --role=roles/dns.admin
 kubectl create secret generic minienv-clouddns-secret --from-file=./clouddns.key.json
 kubectl create -f issuer-prod.yml
 kubectl create -f certificate-standard-prod.yml
