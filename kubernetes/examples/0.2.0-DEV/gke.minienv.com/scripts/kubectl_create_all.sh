836f392fba69aab79a0836cd7725eda1864d20b8
Mark Watson <mark.watson@hotschedules.com>
2018-08-06 01:57:20 -0500

    Redis session store

--- kubernetes/examples/0.2.0-DEV/gke.minienv.com/scripts/kubectl_create_all.sh
+++ kubernetes/examples/0.2.0-DEV/gke.minienv.com/scripts/kubectl_create_all.sh
@@ -4,10 +4,12 @@ kubectl create -f ../daemonset-registry.yml
 kubectl create -f ../daemonset-npm-proxy-cache.yml
 kubectl create -f ../secret-api.yml
 kubectl create -f ../secret-probot.yml
+kubectl create -f ../deployment-redis.yml
 kubectl create -f ../deployment-api.yml
 kubectl create -f ../deployment-web.yml
 kubectl create -f ../deployment-probot.yml
 kubectl create -f ../deployment-env-proxy.yml
+kubectl create -f ../service-redis.yml
 kubectl create -f ../service-api.yml
 kubectl create -f ../service-web.yml
 kubectl create -f ../service-probot.yml
--- kubernetes/examples/0.2.0-DEV/minikube.loc/scripts/kubectl_delete_all.sh
+++ kubernetes/examples/0.2.0-DEV/minikube.loc/scripts/kubectl_delete_all.sh
@@ -5,10 +5,12 @@ kubectl delete service minienv-env-proxy-service
 kubectl delete service minienv-probot-service
 kubectl delete service minienv-web-service
 kubectl delete service minienv-api-service
+kubectl delete service minienv-redis-service
 kubectl delete deployment minienv-env-proxy-deployment
 kubectl delete deployment minienv-probot-deployment
 kubectl delete deployment minienv-web-deployment
 kubectl delete deployment minienv-api-deployment
+kubectl delete deployment minienv-redis-deployment
 kubectl delete secret minienv-probot-secret
 kubectl delete secret minienv-api-secret
 kubectl delete daemonset minienv-npm-proxy-cache-daemonset
